9a0d04a74468fb860b2f018378505ec2435c0d39
Anand Singh <ask26021997@gmail.com>
2022-08-25 14:40:54 +0530

    minor update"

--- src/models/leaderboard.js
+++ src/models/leaderboard.js
@@ -1,13 +1,1 @@
-class Guess {
-  static async create(mongoose) {
-    const leaderBoardSchema = new mongoose.Schema({
-      name: String,
-      score: Number,
-      date: String,
-      position: Number,
-    });
-    return mongoose.model("LeaederBoard", leaderBoardSchema);
-  }
-}
 
-module.exports = Guess.create;
